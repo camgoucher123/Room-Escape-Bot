6db7dc8e77049d05ca68eaafbac456fcef979065
camgoucher123 <32824773+camgoucher123@users.noreply.github.com>
2017-12-29 08:40:06 -0500

    Update app.js

--- app.js
+++ app.js
@@ -294,7 +294,7 @@ var groupId = "3653742"
      	if (shout){
      	roblox.shout(groupId,argsresult.slice(6))
          message.reply("shouted the message:" + argsresult.slice(6))
-	}})
+	}}
 
  });
 client.login(process.env.BOT_TOKEN)
