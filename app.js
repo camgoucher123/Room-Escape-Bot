7cac1484d452f78396653a6eb7402a5170078c79
camgoucher123 <32824773+camgoucher123@users.noreply.github.com>
2017-12-29 00:37:06 -0500

    Update app.js

--- app.js
+++ app.js
@@ -183,10 +183,10 @@ message.channel.send('Pinging!').then(m => m.edit(`Pong! Latency is ${m.createdT
     message.guild.channels.get(general).send(argsresult.slice(4))
   }
 } else
-   if(message.content.startsWith(prefix+'spam')){
-     message.reply("spamming bat")
-     message.guild.members.get("240222986605428746").send("spam")
- } else
+  // if(message.content.startsWith(prefix+'spam')){
+   //  message.reply("spamming bat")
+    // message.guild.members.get("240222986605428746").send("spam")
+ //} else
   if(message.content.startsWith(prefix+'color')){
     let color = args[1];
     if(!color) return message.reply("You must provide a color to choose from! Say \"-listcolors\" To view the colors")
